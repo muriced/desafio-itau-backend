14c19c79efba878fcee7317b12fc402b06242b05
Murilo Cedraz <daucedraz@gmail.com>
2025-05-01 07:07:34 -0300

    commit inicial

---
+++ src/main/java/com/muriced/desafio_itau_backend/DesafioItauBackendApplication.java
@@ -0,0 +1,13 @@
+package com.muriced.desafio_itau_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioItauBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioItauBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muriced/desafio_itau_backend/DesafioItauBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muriced.desafio_itau_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioItauBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
